99437d389cfb755904cd9c417a4290073486fa30
Risto Vatsar <r.vatsar@gmail.com>
2023-02-03 20:07:28 +0200

    week 2 code

---
+++ userAgeValidation/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.WriteLine("Hello, please select your gender (m/f):");
+char userGender = Char.Parse(Console.ReadLine());
+
+Console.WriteLine("Please insert your last name:");
+string lastName = Console.ReadLine();
+
+if (userGender == 'm')
+{
+    Console.WriteLine($"Hello, Mr. {lastName}");
+}
+else if (userGender == 'm')
+{
+    Console.WriteLine($"Hello, Ms. {lastName}!");
+}
+else
+{
+    Console.WriteLine($"Welcome, {lastName}!");
+};
---
+++ userAgeValidation1/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("Please enter your age:");
+int userAge = Int32.Parse(Console.ReadLine());
+
+if (userAge < 13)
+{
+    Console.WriteLine("You are not old enough to use this program!");
+}
+else
+{
+    Console.WriteLine("Welcome to this program!");
+}
